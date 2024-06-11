420f25549d595728774606dd4efdcda1aaed41ec
7minutech <elijohnson1107@gmail.com>
2024-06-11 16:53:56 -0500

    Passed all test for exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(message, repeat_num) {
+    //concates the same message repeat_num amount of times with itself
+    let repeatMessage = message
+    const blankMessage = ""
+    const errorMessage = "ERROR" 
+    if (repeat_num > 0){
+        for (let i = 0; i<repeat_num-1; i++){
+            repeatMessage += message;
+        }  
+        return repeatMessage
+    }
+    //return blank message when number of repeats is 0
+    else if (repeat_num == 0){
+        return blankMessage
+    }
+    //return error message if number of repeats is negative
+    else{
+        return errorMessage
+    }
 };
 
 // Do not edit below this line
