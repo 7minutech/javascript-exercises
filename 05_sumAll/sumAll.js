485615cc5c448ff713488b1f4b0d0d2768e58773
7minutech <elijohnson1107@gmail.com>
2024-06-12 16:43:10 -0500

    Solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(lower_bound,upper_bound) {
+    let sum = 0;
+    let temp = 0;
+    //swap bounds if lower bound is larger than higher bound
+    if (lower_bound>upper_bound){
+        upper_bound = temp
+        upper_bound = lower_bound
+        lower_bound = temp
+    }
+    // if lower bound or upper bound are negative or not a number return error
+    if(lower_bound < 0 || upper_bound < 0 || !(Number.isInteger(lower_bound)) || !(Number.isInteger(upper_bound))){
+        return "ERROR"
+    }
+
+    //sum lower bound to upper bound inclusive
+    for(let i = lower_bound; i<=upper_bound;i++){
+        sum+=i
+    }
+    return sum;
 
 };
 
