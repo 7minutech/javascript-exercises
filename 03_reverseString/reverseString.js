023d8c3079bacfc004fb2c26cb7151541327c152
7minutech <elijohnson1107@gmail.com>
2024-06-12 15:34:43 -0500

    Solved exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArr = []
+    //start at 1 and make it negative for last letter in string
+    for(let i = 1; i<=word.length;i++){
+        wordArr.push(word.at(-i))
+    }
+    return wordArr.join("")
 };
 
 // Do not edit below this line
